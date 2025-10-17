0e11ee827312e7050cef3e74f9a00d8cd3fe8455
sylladev1805 <ousmanespatec@gmail.com>
2025-10-17 17:30:03 +0000

    mises a jour

--- app/Modules/Fixing/Routes/api.php
+++ app/Modules/Fixing/Routes/api.php
@@ -9,7 +9,7 @@ use App\Modules\Fixing\Controllers\InitLivraisonController;
 
 Route::middleware('auth:sanctum')->prefix('v1/fixings')->group(function () {
     Route::apiResource('init-livraisons', InitLivraisonController::class);
-     Route::apiResource('clients', FixingClientController::class);
+     Route::apiResource('clients-fixings', FixingClientController::class);
 
     Route::get('fixing-fournisseurs/restore/{id}', [FixingController::class, 'restore']);
     Route::put('fixing-fournisseurs/status/{id}', [FixingController::class, 'status']);
