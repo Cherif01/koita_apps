9ea9c80e4a53961a9a9cb2fa6414843d255b843f
sylladev1805 <ousmanespatec@gmail.com>
2025-10-20 15:12:45 +0000

    mises a jour

--- app/Modules/Settings/Resources/ClientResource.php
+++ app/Modules/Settings/Resources/ClientResource.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Modules\Settings\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -12,25 +11,26 @@ class ClientResource extends JsonResource
     public function toArray($request): array
     {
         return array_filter([
-            'id'              => $this->id,
-            'nom_complet'     => $this->nom_complet,
-            'raison_sociale'  => $this->raison_sociale,
-            'pays'            => $this->pays,
-            'ville'           => $this->ville,
-            'adresse'         => $this->adresse,
-            'telephone'       => $this->telephone,
-            'email'           => $this->email,
+            'id'             => $this->id,
+            'nom_complet'    => $this->nom_complet,
+            'raison_sociale' => $this->raison_sociale,
+            'type_client'    => $this->type_client,
+            'pays'           => $this->pays,
+            'ville'          => $this->ville,
+            'adresse'        => $this->adresse,
+            'telephone'      => $this->telephone,
+            'email'          => $this->email,
 
             // 🔹 Informations d’audit
-            'created_by'      => $this->createur?->name,
-            'modify_by'       => $this->modificateur?->name,
+            'created_by'     => $this->createur?->name,
+            'modify_by'      => $this->modificateur?->name,
 
             // 🔹 Dates formatées
-            'created_at'      => $this->created_at?->format('Y-m-d H:i:s'),
-            'updated_at'      => $this->updated_at?->format('Y-m-d H:i:s'),
+            'created_at'     => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at'     => $this->updated_at?->format('Y-m-d H:i:s'),
 
             // 🔹 Attribut intelligent (nom affichable)
-            'nom_affichage'   => $this->nom_affichage,
-        ], fn($value) => !is_null($value));
+            'nom_affichage'  => $this->nom_affichage,
+        ], fn($value) => ! is_null($value));
     }
 }
